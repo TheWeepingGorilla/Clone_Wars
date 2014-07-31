9f7b18abe91ab191a5b551ca4a30310386490978
Sean Morrison <student@epicodus.com>
2014-07-31 14:21:00 -0700

    Added four classes total.  Also screwed around quite a bit with the inheritance code to see what it would do.  Most of that isn't in the actual committed code but it was a good learning experience :sunglasses:

--- spec/clone_wars_spec.rb
+++ spec/clone_wars_spec.rb
@@ -12,6 +12,16 @@ describe 'Clone' do
     expect(test_clone.health).to eq 10
     expect(test_clone.name).to eq "Generic One"
   end
+  it "saves a copy of each clone made" do
+    Clone.clearClones
+    test_clone = Clone.new({:name => 'Generic One'})
+    expect(Clone.clones[-1].name).to eq "Generic One"
+  end
+  it "saves a copy of clones made in inherited methods" do
+    Clone.clearClones
+    brad1 = Brad.new({:name => 'Brad Zero'})
+    expect(Clone.clones[-1].name).to eq "Brad Zero"
+  end
 end
 
 describe 'Brad' do
@@ -26,6 +36,7 @@ it "Creates a Brad that inherits from the Clone class" do
   brad2 = Brad.new({:name => 'Brad One'})
   expect(Brad.brads[0].name).to eq "Brad Zero"
   expect(Brad.brads[1].name).to eq "Brad One"
+  expect(Brad.brads[1].friend).to eq nil
   end
 end
 
@@ -35,11 +46,41 @@ it "Creates an Angelena that inherits from the Clone class" do
   angelena1 = Angelena.new({:name => 'Angelena Zero'})
   expect(angelena1.health).to eq 10
   end
-  it "save a copy of each brad object to the class array" do
+  it "save a copy of each angelena object to the class array" do
   Angelena.clearAngelenas
   angelena1 = Angelena.new({:name => 'Angelena Zero'})
   angelena2 = Angelena.new({:name => 'Angelena One'})
   expect(Angelena.angelenas[0].name).to eq "Angelena Zero"
   expect(Angelena.angelenas[1].name).to eq "Angelena One"
+  end
+end
+
+describe 'Scarlet' do
+it "Creates a Scarlet that inherits from the Clone class" do
+  Scarlet.clearScarlets
+  scarlet1 = Scarlet.new({:name => 'Scarlet Zero'})
+  expect(scarlet1.health).to eq 10
+  end
+  it "save a copy of each scarlet object to the class array" do
+  Scarlet.clearScarlets
+  scarlet1 = Scarlet.new({:name => 'Scarlet Zero'})
+  scarlet2 = Scarlet.new({:name => 'Scarlet One'})
+  expect(Scarlet.scarlets[0].name).to eq "Scarlet Zero"
+  expect(Scarlet.scarlets[1].name).to eq "Scarlet One"
+  end
+end
+
+describe 'Bill' do
+it "Creates a Bill that inherits from the Clone class" do
+  Bill.clearBills
+  bill1 = Bill.new({:name => 'Bill Zero'})
+  expect(bill1.health).to eq 10
+  end
+  it "save a copy of each bill object to the class array" do
+  Bill.clearBills
+  bill0 = Bill.new({:name => 'Bill Zero'})
+  bill1 = Bill.new({:name => 'Bill One'})
+  expect(Bill.bills[0].name).to eq "Bill Zero"
+  expect(Bill.bills[1].name).to eq "Bill One"
   end
 end
